295a8f1e49d77925d88a9f26709dc0f884988650
Roy Daniel <roydanielr@gmail.com>
2022-04-18 20:25:30 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001
