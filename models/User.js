857a6fba0617e2348be612cf1ebfee0c6d2216fb
Roy Daniel <roydanielr@gmail.com>
2022-04-21 07:51:42 -0400

    user model

--- models/User.js
+++ models/User.js
@@ -1,4 +1,10 @@
+import { toHaveFocus } from "@testing-library/jest-dom/dist/matchers";
 import { Schema } from "../config/database";
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt')
+
+const SALT_ROUNDS = 6
 
 const userSchema = new Schema({
     name: {type: String, required: true},
@@ -15,4 +21,21 @@ const userSchema = new Schema({
         minLength: 3,
         required: true
     }
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: function(doc, ret) {
+            delete ret.password
+            return ret
+        }
+    }
 })
+
+userSchema.pre('save', async function(next) {
+    // The user doc
+    if(!this.isModified('password')) return next() // IF the user changed the password
+    this.password = await bcrypt.hash(this.password, SALT_ROUNDS) // Update the password with the computed hash
+    return next()
+})
+
+module.exports = mongoose.model('User', userSchema)
