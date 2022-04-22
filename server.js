b276feb0fe1201934d143eda3be63b5683067310
Roy Daniel <roydanielr@gmail.com>
2022-04-22 13:38:35 -0400

    add jsonwebtoken and server update

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 require('dotenv').config()
-const express = require('express')
-const path = require('path')
-const logger = require('morgan')
 const PORT = process.env.PORT || 3001
-
+const path = require('path')
+const express = require('express')
+const logger = require('morgan')
 require('./config/database.js')
 
 const app = express()
-
-
 
 // middleware
 app.use(logger('dev')) // Morgan
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'build')))
 
-// API
+// Check if token and create req.user
+app.use(require('./config/checkToken'))
 
+// API middleware
+app.use('/api/users', require('./routes/api/users'))
+
+// Catch All Route
 app.get('.*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
