e2ae32c2c59e1c88efd0ef52a3b1efa1093ec14a
Roy Daniel <roydanielr@gmail.com>
2022-04-25 00:52:06 -0400

    time tracker

--- src/components/TimeTracker/TimeTracker.jsx
+++ src/components/TimeTracker/TimeTracker.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from 'react'
 
 export default function TimeTracker() {
+    const [seconds, setSeconds] = useState(900)
+    const INTERVAL_MS = 5000
+
+    const countDown = (newSeconds) => {
+        setTimeout(() => {
+            setSeconds(newSeconds - 1)
+            console.log(`seconds = ${seconds}`)
+            console.log(new Date(newSeconds * 1000).toISOString().substr(14, 5))
+            if(newSeconds > 0) {
+                return
+                countDown(newSeconds)
+            } else {
+                console.log(`time!`)
+            return
+            }
+        }, INTERVAL_MS);
+    }
+
+    useEffect(() => {
+        countDown(seconds)
+    }, [])
+    
     return (
         <div>
             <h3>Time Tracking</h3>
+            <h3>{new Date(seconds * 1000).toISOString().substr(14, 5)}</h3>
         </div>
     )
 }
